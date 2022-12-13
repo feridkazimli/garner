3d191979b02aa2240387606d2d120aea116e1b46
elmuradhasan <hsnlimurad540@gmail.com>
2022-12-13 06:13:19 -0800

    Add forgotten password component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Forgot from './components/Login/ForgottenPassword/Forgot';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import Home from './components/Home/Home';
 function App() {
   return (
    <>
+    <Router>
+        <Routes>
+        <Route path="/"  element={<Home/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />}  />        
+          <Route path="/forgotpass" element={<Forgot />}  />        
+        </Routes>
+    </Router>
    </>
   );
 }
