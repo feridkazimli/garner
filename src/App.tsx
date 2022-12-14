bc90a0c1a2bb5fbc145499d11bae90bf806c61ce
elmuradhasan <hsnlimurad540@gmail.com>
2022-12-14 07:44:19 -0800

    end bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, lazy, Suspense } from "react";
+import React, { Suspense } from "react";
 import routes from "./Router/index"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RouteType } from "./Router/AuthRouter";
